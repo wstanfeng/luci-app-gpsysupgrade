f9593185ecc76591983cc2ec2059aff8c4a6b946
Tan Feng <wstanfeng@gmail.com>
2024-10-20 08:21:07 +0800

    Update sysupgrade.lua

--- luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
+++ luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
@@ -6,8 +6,8 @@ local i18n = require "luci.i18n"
 local ipkg = require("luci.model.ipkg")
 local api = require "luci.model.cbi.gpsysupgrade.api"
 local Variable1 = "wstanfeng"
-local Variable2 = "OpenWRT-CI"
-local Variable3 = "X86_64"
+local Variable2 = "Actions-OpenWrt"
+local Variable3 = "CI"
 
 function get_system_version()
 	local system_version = luci.sys.exec("[ -f '/etc/openwrt_version' ] && echo -n `cat /etc/openwrt_version` | tr -d '\n'")
